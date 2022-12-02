9309b68b6fa868cfbcafed1e6d7f18dacb4092be
Paulo Nova <paulovilanova@MacBook-Pro-som-tillhor-Paulo.local>
2022-12-02 12:30:28 +0100

    The base

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
+import client from "../apollo/client";
+//import GET_POSTS from "../queries/get-menus";
+import { gql } from "@apollo/client";
 
 export default function Home() {
-  const data = fetch(process.env.WORDPRESS_LOCAL_API_URL, {
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-    body: JSON.stringify({ test: "value" }),
-  });
-
-  console.log("Data: ", data);
   return (
     <div className={styles.container}>
       <Head>
@@ -24,4 +20,25 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const { data, loading, networkStatus } = await client.query({
+    query: gql`
+            posts {
+              edges {
+                node {
+                  title
+                  slug
+                  date
+                }
+              }
+            }
+          `,
+  });
+
+  console.log("DATA: ", data);
+  return {
+    props: {}, // will be passed to the page component as props
+  };
 }
--- src/queries/get-menus.js
+++ src/queries/get-menus.js
@@ -1,13 +1,27 @@
 import { gql } from "@apollo/client";
-import MenuFragment from "./fragments/menus";
+//import MenuFragment from "./fragments/menus";
 
 /**
  * Here I create a Fragment in GraphQl and the tutorial is here:
  * https://codeytek.com/are-you-using-fragments-in-graphql/
  */
 
-const GET_MENUS = gql`
-headerMenus: menuItems(where: {location: HCMS_MENU_HEADER, parentId: "0"}) {
+const GET_POSTS = gql`
+  posts {
+    edges {
+      node {
+        title
+        slug
+        date
+      }
+    }
+  }
+`;
+
+export default GET_POSTS;
+
+/**
+ * headerMenus: menuItems(where: {location: HCMS_MENU_HEADER, parentId: "0"}) {
   edges {
     node {
       ...MenuFragment
@@ -39,6 +53,4 @@ footerMenus: menuItems(where: {location: HCMS_MENU_FOOTER, parentId: "0"}) {
   }
 }
 ${MenuFragment}
-`;
-
-export default GET_MENUS;
+ */
